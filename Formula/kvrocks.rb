a586ea881f7ef61eaabd56389b5e0475368951d6
hulk <hulk.website@gmail.com>
2021-05-19 12:32:42 +0800

    Fix some issues were found in code review

--- Formula/kvrocks.rb
+++ Formula/kvrocks.rb
@@ -4,25 +4,35 @@ class Kvrocks < Formula
   url "https://github.com/bitleak/kvrocks.git",
     tag:      "v2.0.1",
     revision: "5f41145adb25a79706a68d233a28a0f633ef255f"
-
   license "BSD-3-Clause"
-
   head "https://github.com/bitleak/kvrocks.git", branch: "unstable"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
-  depends_on "gcc" => :build
   depends_on "libtool" => :build
 
   def install
-    mkdir_p "_build"
-    system "sh", "build.sh", "_build"
-    bin.install "_build/kvrocks"
-    etc.install "kvrocks.conf"
+    system "git", "submodule", "init"
+    system "git", "submodule", "update"
+    mkdir "build" do
+      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "..", *std_cmake_args
+      system "make", "-j4", "kvrocks"
+      bin.install "kvrocks"
+      etc.install "../kvrocks.conf"
+    end
   end
 
   test do
-    assert_match "/Version: 999\.999\.999.*/", shell_output("#{bin}/kvrocks -v 2>&1")
+    assert_match "Version: 2.0.1", shell_output("#{bin}/kvrocks -v 2>&1")
+    kvrocks = fork do
+      exec bin/"kvrocks"
+    end
+    sleep(1)
+    output = shell_output("echo \"ping\\r\\n\"|nc 127.0.0.1 6666")
+    assert_match "+PONG", output
+    ensure
+      Process.kill(15, kvrocks)
+      Process.wait kvrocks
   end
 end
